38cbe53028662412c532e578d4236c335becafa8
Mustafa Alsihati <51824138+MustafaAlsihati@users.noreply.github.com>
2023-07-08 17:03:52 +0300

    Support ESM & CJS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,13 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
+  'parser': '@typescript-eslint/parser',
+  'extends': [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  'parserOptions': {
+    'sourceType': 'module',
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-var-requires': 0,
-    quotes: 0,
-    semi: ['error', 'always'],
-    'linebreak-style': 0,
-    indent: 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-unused-vars': 1,
-    'prefer-const': 0,
-    'no-prototype-builtins': 0,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-non-null-asserted-optional-chain': 0,
-  },
+  'plugins': [
+    '@typescript-eslint',
+  ]
 };
