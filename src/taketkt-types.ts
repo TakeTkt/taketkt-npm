b4fcb9b28f5d89b13569de8d59b8a11771232cac
Mustafa Alsihati <51824138+MustafaAlsihati@users.noreply.github.com>
2024-11-01 12:27:46 +0300

    Fixes

--- src/taketkt-types.ts
+++ src/taketkt-types.ts
@@ -1,4 +1,4 @@
-import type { Decimal } from '@prisma/client/runtime';
+import { Decimal } from '@prisma/client/runtime/library';
 import type { Currency } from './currencies';
 import type { TimeZones } from './timezones';
 
