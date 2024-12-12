4bb2e8e77b8c76fee81015337d1d50d58b025a64
Mustafa Alsihati <51824138+MustafaAlsihati@users.noreply.github.com>
2024-12-12 14:05:36 +0300

    Fix import from main

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './roles';
 export * from './permissions';
 export * from './setters';
 export * from './payment';
+export * from './calculations';
 
 import * as Types from './taketkt-types';
 import * as Utils from './utils';
@@ -11,8 +12,9 @@ import * as Roles from './roles';
 import * as Permissions from './permissions';
 import * as Setters from './setters';
 import * as Payment from './payment';
+import * as Calculations from './calculations';
 
-export { Types, Utils, Roles, Permissions, Setters, Payment };
+export { Types, Utils, Roles, Permissions, Setters, Payment, Calculations };
 
 // export * as Types from './taketkt-types';
 // export * as Utils from './utils';
